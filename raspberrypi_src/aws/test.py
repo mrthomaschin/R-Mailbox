7eb5d631a075a92ec6b99d4aea0457bcdcd34939
Michael De Sousa <mdeso001@ucr.edu>
2020-05-13 14:46:19 -0700

    Added working demo of AWS/SPI integration for RPi and Atmega

--- raspberrypi_src/aws/test.py
+++ raspberrypi_src/aws/test.py
@@ -9,16 +9,16 @@ ask = Ask(app, '/')
 @ask.intent('BlinkIntent')
 def blink():
   print "LED on"
-  GPIO.output(18, GPIO.HIGH)
+  GPIO.output(21, GPIO.HIGH)
   time.sleep(1)
   print "LED off"
-  GPIO.output(18, GPIO.LOW)
-  return statement('Blinky blink')
+  GPIO.output(21, GPIO.LOW)
+  return statement('Flashing Light on R mailbox')
  
 if __name__ == '__main__':
   try:
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
+    GPIO.setup(21, GPIO.OUT)
     app.run(debug=True)
   finally:
     GPIO.cleanup()
