ebb9c23d24ea9a1d3a01e230bb8a4b65b8768003
Jacob Halvorson <jhalvorson6687@icloud.com>
2020-05-20 15:12:34 -0700

    removing old files

--- raspberrypi_src/__init__.py
+++
@@ -1,2 +0,0 @@
-# This file is to ensure that the folder where this file is present is considered as package by
-# Python which will help us in importing files and methods from files present in this folder
--- raspberrypi_src/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-TWILIO_SID = os.getenv("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH = os.getenv("TWILIO_AUTH_TOKEN")
