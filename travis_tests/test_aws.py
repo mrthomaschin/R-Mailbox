b1b50800ff4ef041db8671463a9d36177df46e4d
Thomas Chin <thomasc246@gmail.com>
2020-05-24 22:33:31 -0700

    Add basic SPI unit test

--- travis_tests/test_aws.py
+++ travis_tests/test_aws.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append("..")
 import fake_rpi
-import unittest
-from unittest.mock import Mock, MagicMock, patch
+from unittest import mock
+from unittest.mock import MagicMock, patch
 
 sys.modules['RPi'] = fake_rpi.RPi  # Fake RPi
 sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO  # Fake GPIO
@@ -10,20 +10,31 @@ sys.modules['spidev'] = MagicMock()
 
 # from travis_tests.aws_tst import default
 # from raspberrypi_src.component_files.tst import default
-from raspberrypi_src.component_files.aws_spi_tst import default, app, isMail
+from raspberrypi_src.component_files.aws_spi_tst import default, isMail
 
 # def test_default():
 #     a = 1
 #     b = 2
-#     assert a + b == 3, "test failed" s
+#     assert a + b == 3, "test failed"
 
 
 def test_default():
     assert default(1, 2) == 3, "test failed"
 
 
-# Raspberry Pi sends a statement to Alexa
-def test_aws():
+# Raspberry Pi does not detect package, sends a statement to Alexa
+def test_aws_command_no():
     statement_mock = isMail()
     phrase = statement_mock._response['outputSpeech']['text']
     assert phrase == "There is currently no mail", "test failed"
+
+
+# # Raspberry Pi detects package, sends a statement to Alexa
+# def test_aws_command_yes():
+#     sys.modules['spidev'].SpiDev.readbytes.return_value = 1
+
+#     statement_mock = isMail()
+#     phrase = statement_mock._response['outputSpeech']['text']
+#     print("........")
+#     print(phrase)
+#     assert phrase == "There is currently no mail", "test failed"
