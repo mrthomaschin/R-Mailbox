164b4d9c363c623293bf6a6243d78b4c339bb70e
Thomas Chin <thomasc246@gmail.com>
2020-05-26 16:34:22 -0700

    testing synax error

--- raspberrypi_src/notifications/send_sms.py
+++ raspberrypi_src/notifications/send_sms.py
@@ -39,6 +39,7 @@ def sendSMS(to, from_, message):
                 to=to
             )
     except TwilioRestException as e:
-        logging.error(f'Error: {e}')
+        # logging.error(f'Error: {e}\\')
+        logging.error(f'Error: \\')
         return
     return sent_message.sid
