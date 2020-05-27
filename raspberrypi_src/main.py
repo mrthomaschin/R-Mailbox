ab878dbce26d069af16c286448705695a23e9f71
Michael De Sousa <mdeso001@ucr.edu>
2020-05-26 21:36:10 -0700

    Full "Request to Open" functionality added

--- raspberrypi_src/main.py
+++ raspberrypi_src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from multiprocessing import Process
 import RPi.GPIO as GPIO
 
 #Alexa
@@ -10,54 +11,9 @@ import time
 from twilio.twiml.messaging_response import MessagingResponse
 from notifications import send_sms
 
-access = 0
-
-def test():
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(20, GPIO.OUT)
-	while (True):
-		input = GPIO.input(21)
-		if input == False:
-			print("Button Pressed")
-			send_sms.sendSMS('+19098272197', '+12058329927', "Request to open R'Mailbox: Reply with Y/y or N/n.")
-			print(access)
-
-		if access == 1:
-			GPIO.output(20, 1)
-			print("Door Opened")
-
-		elif access == 0:
-			GPIO.output(20, 0)
-
-app = Flask(__name__)
-ask = Ask(app, '/')
-
-@app.route("/sms", methods=['GET', 'POST'])
-def sms_reply():
-    # Get message body of incoming message
-    body = request.values.get('Body', None)
-
-    # Start Response
-    resp = MessagingResponse()
-
-    # Determine Correct Response
-    if body == 'Y' or body == 'y':
-        access = 1
-        resp.message("Access granted to R'Mailbox.")
-    elif body == 'N' or body == 'n':
-        access = 0
-        resp.message("Access denied to R'Mailbox.")
-    else:
-        resp.message("Please respond with Y/y or N/n")
-
-    return str(resp)
-
 # for testing purposes
 def default(a, b):
     return a + b
-
 
 def createSPI(bus, device):
     spi = spidev.SpiDev()
@@ -67,42 +23,66 @@ def createSPI(bus, device):
     spi.mode = 0
     return spi
 
+def requestButton():
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	while (True):
+		input = GPIO.input(21)
+		if input == False:
+			print("Button Pressed")
+			send_sms.sendSMS('+19098272197', '+12058329927', "Request to open R'Mailbox: Reply with Y/y or N/n.")
+		time.sleep(1)
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+	# Get message body of incoming message
+	body = request.values.get('Body', None)
+
+	# Start Response
+	resp = MessagingResponse()
+
+	# Determine Correct Response
+	if body == 'Y' or body == 'y':
+		print('Unlocking door')
+
+		#SPI Command to Atmega
+		Unlock_Door = 0x20
+
+		atmegaSPI.xfer([Unlock_Door])
+
+		resp.message("Access granted to R'Mailbox.")
+	elif body == 'N' or body == 'n':
+		resp.message("Access denied to R'Mailbox.")
+	else:
+		resp.message("Please respond with Y/y or N/n")
+
+	return str(resp)
 
 @ask.intent('IRIntent')
 def isMail():
-    atmegaSPI = createSPI(0, 0)
-    print('Getting IR sensor value')
+	print('Getting IR sensor value')
 
-    Send_Status = 0x10
+	#SPI Command to Atmega
+	Send_Status = 0x10
 
-    atmegaSPI.xfer([Send_Status])
-    sensorValue = atmegaSPI.readbytes(1)[0]
+	atmegaSPI.xfer([Send_Status])
+	sensorValue = atmegaSPI.readbytes(1)[0]
 
-    print("Current Sensor Status: ", sensorValue)
+	print("Current Sensor Status: ", sensorValue)
 
-    atmegaSPI.close()
+	if sensorValue == 1:
+    		return statement('You have mail')
 
-    if sensorValue == 1:
-        return statement('You have mail')
-
-    return statement('There is currently no mail')
+	return statement('There is currently no mail')
 
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
 
-    # GPIO, for debugging purposes
-    # Pin 21 is the bottom right pin, to the left of it is GND for easy hookup
-    # GPIO.setmode(GPIO.BCM)
-    # GPIO.setup(21, GPIO.OUT)
-
-    #try:
-        #atmegaSPI = createSPI(0, 0)
-
-        #app.run(debug=True)
-
-    #except KeyboardInterrupt:
-       # exit()
-    # finally:
-    #     # GPIO.cleanup()
-if __name__ == "__main__":
-    app.run(debug=True)
+	atmegaSPI = createSPI(0, 0)
+	Process(target=app.run, kwargs=dict(debug=True)).start()
+	Process(target=requestButton).start()
+	atmegaSPI.close()
