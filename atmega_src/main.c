6a208ad5e3311457adc20f8b2954c9dd21b9d2fd
Thomas Chin <thomasc246@gmail.com>
2020-06-02 00:05:37 -0700

    Testing C unit tests inside travis_tests directory

--- atmega_src/main.c
+++ atmega_src/main.c
@@ -9,7 +9,7 @@
 #include "main.h"
 #include "spi.h"
 
-#include "test_atmega.h"
+#include "../travis_tests/test_atmega.h"
 
 //PC1 is authorization signal from security Pi, Debug Button on PA0 mocks authorization signal to unlock
 #define AUTHORIZED (~PINC & 0x02) | (~PINA & 0x01)
